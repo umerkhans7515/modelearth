a6345c45a006249045351e1c3412b37595f9f24a
Umer Ali <48864352+umerkhans7515@users.noreply.github.com>
2022-08-15 13:31:25 +0500

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import Format from '../layout/format'
+
+//components
+
+import Section1 from '../components/section1'
+import Section2 from '../components/section2'
+
+export default function Home() {
+  return (
+    <Format>
+        <Section1> </Section1>
+        <Section2> </Section2>
+    </Format>
+   
+  
+  )
+}
+
+
---
+++ pages/posts/Waterheater.js
@@ -0,0 +1,17 @@
+import Format from '../../layout/format'
+
+export default function Page() {
+return(
+    <Format>
+     <section className="container mx-auto md:px-2 py-16 w-1/2">
+
+        <div className="post py-10"> 
+        <h1 className="font-bold text-4xl text-center pb-5 ">Best Tankless Water Heater for 2022 </h1>
+        <p className="text-xl text-center">
+            If you are looking for a solution to freezing cold water then Tankless Water heater is great option to use
+        </p>
+        </div>
+     </section>
+    </Format>
+)    
+};
